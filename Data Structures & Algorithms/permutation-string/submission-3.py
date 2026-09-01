d669a62f380d37c9d648af8a12a3393e574c6908
Gabriel Yuan <gabeyuan7@gmail.com>
2026-08-31 23:54:22 -0700

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.py
@@ -0,0 +1,28 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        windowLength = len(s1)
+        start = 0 
+        end = windowLength
+        permChars = {}
+        for ch in s1:
+            if ch in permChars:
+                permChars[ch] += 1
+            else: 
+                permChars[ch] = 1
+        while end <= len(s2):
+            substring = s2[start:end]
+            substringChars = {}
+            for ch in substring:
+                if ch in substringChars:
+                    substringChars[ch] += 1
+                else: 
+                    substringChars[ch] = 1
+            if permChars == substringChars:
+                return True
+            start += 1
+            end += 1
+        return False
+
+
+
+
